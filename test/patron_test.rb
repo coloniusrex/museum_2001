abea14027b743e8158ca4453df1eb21a4cb28404
Colin Alexander <colintalex@gmail.com>
2020-02-19 09:16:55 -0700

    Adds addtl assert to 2nd test.

--- test/patron_test.rb
+++ test/patron_test.rb
@@ -17,5 +17,6 @@ class PatronTest < Minitest::Test
   def test_patron_has_attributes
     assert_equal "Bob", @patron_1.name
     assert_equal 20, @patron_1.spending_money
+    assert_equal [], @patron_1.interests
   end
 end
