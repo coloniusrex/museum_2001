45c2bbd20391f4af33dea58ccacbd82b1579edb0
Colin Alexander <colintalex@gmail.com>
2020-02-19 10:55:11 -0700

    Adds logic for oatrons_by_exhibit method.

--- lib/museum.rb
+++ lib/museum.rb
@@ -1,8 +1,9 @@
 class Museum
-    attr_reader :name, :exhibits
+    attr_reader :name, :exhibits, :patrons
   def initialize(name)
     @name = name
     @exhibits = []
+    @patrons = []
   end
 
   def add_exhibit(exhibit)
@@ -11,6 +12,17 @@ class Museum
 
   def recommend_exhibits(patron)
     exhibits.find_all { |exhibit| patron.interests.include?(exhibit.name)}
+  end
+
+  def admit(patron)
+    @patrons << patron
+  end
+
+  def patrons_by_exhibit_interest
+    @exhibits.inject({}) do |memo, exhibit|
+      memo[exhibit] = Array.new
+      memo
+    end
   end
 
 end
